c5a6fca1a2f5298d88e4aaadfa1c0ca07e41397b
Cecilia1202 <102317330+Cecilia1202@users.noreply.github.com>
2022-03-25 01:20:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+const element = document.getElementById("index.html");
+element.addEventListener("push", About);
+
+function myFunction() {
+  document.getElementById("index").innerHTML = "page2.html";
+}
+</script>
+       
